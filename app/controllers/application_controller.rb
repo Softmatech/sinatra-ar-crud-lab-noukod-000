d103b63df85b2118b6381adc041a1f6f1975fff1
Andy Feidje Joseph <ingandy75@gmail.com>
2019-07-09 05:47:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  delete '/articles/:id/delete' do
+  delete '/posts/:id/delete' do
     Articles.delete params[:id]
     redirect '/articles'
   end
